28afd57fbdc2e254aece2da33b4d0a52f94090ee
Austin Sinclair <sinclaau@sheridancollege.ca>
2025-02-10 16:33:31 -0500

    Uploading Provided Base Code
    These are the 3 files that were given to start the project. Throughout the term we will be converting and adding to these files to create a functioning Crazy Eights Card Game.

---
+++ Card.java
@@ -0,0 +1,39 @@
+package cardgame;
+
+
+
+public class Card {
+    //Example using Enum
+    public enum Suit {
+        HEARTS, CLUBS, SPADES, DIAMONDS
+    }
+
+    public enum Value {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING,
+        JACL
+    }
+    private Value value;
+    private Suit suit;
+
+    public Card(Value v, Suit s) {
+        value = v;
+        suit = s;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+}
